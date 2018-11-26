d1719af10d4cb8ef26500b621115c18ee92d1d00
Dieter Holvoet <dieter.holvoet@gmail.com>
2018-11-26 12:41:46 +0100

    Fixed default metadata preview settings

--- src/Controller/PreviewModalController.php
+++ src/Controller/PreviewModalController.php
@@ -85,6 +85,14 @@ class PreviewModalController extends ControllerBase
         $settings = [
             'google_div' => '#seopreview-google',
             'facebook_div' => '#seopreview-facebook',
+            'metadata' => [
+                'title' => '',
+                'desc' => '',
+                'url' => [],
+            ],
+            'facebook' => [
+                'featured_image' => '',
+            ],
         ];
 
         $entity = $this->urlHelper->getRefererEntity();
