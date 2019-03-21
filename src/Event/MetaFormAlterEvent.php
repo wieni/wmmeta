15c21d4e0dde6bff0d1cae1444eec3460688748c
Dieter Holvoet <dieter.holvoet@gmail.com>
2019-03-21 14:07:42 +0100

    Pass the parent form state to MetaFormAlterEvent

--- src/Event/MetaFormAlterEvent.php
+++ src/Event/MetaFormAlterEvent.php
@@ -2,26 +2,43 @@
 
 namespace Drupal\wmmeta\Event;
 
+use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\EventDispatcher\Event;
 
 class MetaFormAlterEvent extends Event
 {
     /** @var array */
     protected $form;
+    /** @var FormStateInterface */
+    protected $parentFormState;
 
-    public function __construct(array &$form)
-    {
+    public function __construct(
+        array &$form,
+        FormStateInterface $parentFormState
+    ) {
         $this->form = &$form;
+        $this->parentFormState = $parentFormState;
     }
 
     /**
      * Get the inline entity form.
      *
      * @return array
-     *   The usages.
+     *   The entity form.
      */
     public function &getForm(): array
     {
         return $this->form;
+    }
+
+    /**
+     * Get the form state of the parent form.
+     *
+     * @return FormStateInterface
+     *   The form state.
+     */
+    public function getParentFormState(): FormStateInterface
+    {
+        return $this->parentFormState;
     }
 }
