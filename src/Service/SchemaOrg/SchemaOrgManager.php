35c1de4ab058db3e25b5e8c4aefd1e3fb50691c2
Dieter Holvoet <dieter.holvoet@gmail.com>
2019-04-15 10:35:41 +0200

    Disable Schema.org tags by default

--- src/Service/SchemaOrg/SchemaOrgManager.php
+++ src/Service/SchemaOrg/SchemaOrgManager.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\wmmeta\Service\SchemaOrg;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Routing\AdminContext;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\hook_event_dispatcher\Event\Preprocess\HtmlPreprocessEvent;
@@ -13,6 +14,8 @@ class SchemaOrgManager implements EventSubscriberInterface
     /** @var SchemaProviderInterface[] */
     protected $providers = [];
 
+    /** @var ConfigFactoryInterface */
+    protected $configFactory;
     /** @var RouteMatchInterface */
     protected $routeMatch;
     /** @var AdminContext */
@@ -26,9 +29,11 @@ class SchemaOrgManager implements EventSubscriberInterface
     }
 
     public function __construct(
+        ConfigFactoryInterface $configFactory,
         RouteMatchInterface $routeMatch,
         AdminContext $adminContext
     ) {
+        $this->configFactory = $configFactory;
         $this->routeMatch = $routeMatch;
         $this->adminContext = $adminContext;
     }
@@ -44,7 +49,10 @@ class SchemaOrgManager implements EventSubscriberInterface
     {
         $elements = [];
 
-        if ($this->adminContext->isAdminRoute()) {
+        if (
+            $this->adminContext->isAdminRoute()
+            || !$this->configFactory->get('wmmeta.settings')->get('schema_org_tags')
+        ) {
             return;
         }
 
